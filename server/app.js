7354ee1e031214cc3c6b09c99f712ee8ebd00bf3
Alex Moores <moores.alexd@gmail.com>
2017-03-28 22:55:28 -0400

    added path to app.js file

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 var app = express();
 app.use(express.static(path.join(__dirname, '../client')));
