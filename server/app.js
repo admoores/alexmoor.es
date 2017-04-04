2e7741a19235d3c55980b0fc65a831c970e5f4f6
Alex Moores <moores.alexd@gmail.com>
2017-04-03 22:31:05 -0400

    added test vhost functionality

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
-// var vhost = require('vhost');
+var vhost = require('vhost');
 
 
 var app = express();
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(vhost('boogiebox.alexmoor.es', require('/var/www/boogie-box/server/app.js').app));
+app.use(vhost('boogiebox.alexmoor.es', require('/var/www/boogie-box/server/app.js').app));
 
 app.get('/', function(req, res) {
   res.json({string: 'this is a string'});
