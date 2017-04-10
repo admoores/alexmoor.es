3d1bdd40dd074bd87a19e741d7fc3fc68eb95b35
Alex Moores <moores.alexd@gmail.com>
2017-04-10 02:25:44 -0400

    trying express-http-proxy

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const path = require('path');
 const morgan = require('morgan');
 const fs = require('fs');
 const https = require('https');
+const proxy = require('express-http-proxy');
 // const httpProxy = require('http-proxy');
 
 // const proxy = httpProxy.createProxyServer();
@@ -28,6 +29,7 @@ app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/boogiebox', proxy('http://localhost:8080'));
 
 app.get('/', (req, res) => {
   res.json({string: 'this is a string'});
