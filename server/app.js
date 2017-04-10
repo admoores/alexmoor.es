addc912c92bb588122bde8d964dacad44bad008a
Alex Moores <moores.alexd@gmail.com>
2017-04-09 22:16:17 -0400

    added redirect for http requests to the https addresses

--- server/app.js
+++ server/app.js
@@ -9,12 +9,20 @@ const https = require('https');
 const port = 443;
 
 const app = express();
+
+const httpRedirect = express.createServer();
+httpRedirect.get('*', (req, res) => {
+  res.redirect('https://alexmoor.es' + req.url);
+  console.log('HTTP request made, redirected to HTTPS');
+});
+httpRedirect.listen(80);
+
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.json({string: 'this is a string'});
 });
 
@@ -23,8 +31,6 @@ const cert = fs.readFileSync('/etc/letsencrypt/live/alexmoor.es//fullchain.pem')
 const ca = fs.readFileSync('/etc/letsencrypt/live/alexmoor.es/chain.pem');
 const server = https.createServer({key: pKey, cert: cert, ca: ca}, app)
 
-server.listen(port);
-
-// app.listen(port, function() {
-//   console.log('listening on port', port)
-// });
+server.listen(port, () => {
+  console.log('Securely listening on port', port);
+});
