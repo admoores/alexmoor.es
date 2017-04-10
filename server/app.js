409dd0eda6fb2d3f3ab38df90f389d7506c7df73
Alex Moores <moores.alexd@gmail.com>
2017-04-09 23:52:26 -0400

    testing serving of static assets without truncating of url

--- server/app.js
+++ server/app.js
@@ -29,7 +29,6 @@ app.get('/', (req, res) => {
 });
 
 app.all('/boogiebox*', (req, res) => {
-  req.url = req.url.slice(10);
   console.log('redirecting to boogiebox', req.url);
   proxy.web(req, res, {target: 'http://localhost:8080'}, (error) => {
     console.log('Error on getting external app');
