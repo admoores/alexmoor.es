9e0beea21660fd84e5e4f214f23b45eb8891c15f
Alex Moores <moores.alexd@gmail.com>
2017-04-09 21:57:10 -0400

    migrated to use of https

--- server/app.js
+++ server/app.js
@@ -1,25 +1,27 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
-var vhost = require('vhost');
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
+const port = 443;
 
-var app = express();
+const app = express();
+app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(vhost('/boogie-box', require('/var/www/boogie-box/server/app.js').app));
 
 app.get('/', function(req, res) {
   res.json({string: 'this is a string'});
 });
 
+const pKey = fs.readFileSync('/etc/letsencrypt/live/alexmoor.es/privkey.pem');
+const cert = fs.readFileSync('/etc/letsencrypt/live/alexmoor.es//fullchain.pem');
+const ca = fs.readFileSync('/etc/letsencrypt/live/alexmoor.es/chain.pem');
+const server = https.createServer({key: pKey, cert: cert, ca: ca}, app)
 
+server.listen(port);
 
-
-
-var port = 90;
-app.listen(port, function() {
-  console.log('listening on port', port)
-});
+// app.listen(port, function() {
+//   console.log('listening on port', port)
+// });
