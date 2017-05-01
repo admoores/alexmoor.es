89a709679a6700a63729978f3db75bf4e9c6a7eb
Alex Moores <moores.alexd@gmail.com>
2017-05-01 03:20:36 -0400

    testing flexbox stuff

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+let lipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pulvinar justo non tempor rhoncus. Aenean pretium, ante vitae sollicitudin dignissim, diam urna placerat tortor, et malesuada orci sapien at nisi. Nullam tincidunt felis nisl, eu convallis turpis scelerisque vel. Etiam ornare, nibh eget dignissim porttitor, mi turpis varius lectus, lobortis pulvinar ex ipsum sit amet augue. Sed molestie placerat est in lobortis. Praesent eget magna in tellus semper fermentum id sit amet ipsum. Nam quis molestie ante, sed bibendum nisi.';
+
 class App extends React.Component {
   render() {
     return (
       <div className="container">
         <div className="block">
-          <p>Hello</p>
+          <p>{lipsum}</p>
         </div>
         <div className="block">
-          <p>World</p>
+          <p>{lipsum}</p>
         </div>
         <div className="block">
-          <p>Flexy</p>
+          <p>{lipsum}</p>
         </div>
       </div>
     );
