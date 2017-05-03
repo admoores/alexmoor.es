b3b4433914a6be279a0d6e5bad7f43aa7b708b73
Alex Moores <moores.alexd@gmail.com>
2017-05-03 18:18:06 -0400

    navBar component

---
+++ client/src/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div className="navBar">
+        <a href='/index.html' className="navElement">HOME</a>
+        <a href='/portfolio/index.html' className="navElement">PORTFOLIO</a>
+      </div>
+    );
+  }
+}
+
+export NavBar;
