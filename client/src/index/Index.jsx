a2a95f74498b5520c3fd28208559b652f31063d3
Alex Moores <moores.alexd@gmail.com>
2017-05-03 18:28:31 -0400

    navBar pathing issue

--- client/src/index/Index.jsx
+++ client/src/index/Index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
-import NavBar from '../NavBar';
+import NavBar from '../NavBar.jsx';
 
 let lipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pulvinar justo non tempor rhoncus. Aenean pretium, ante vitae sollicitudin dignissim, diam urna placerat tortor, et malesuada orci sapien at nisi. Nullam tincidunt felis nisl, eu convallis turpis scelerisque vel. Etiam ornare, nibh eget dignissim porttitor, mi turpis varius lectus, lobortis pulvinar ex ipsum sit amet augue. Sed molestie placerat est in lobortis. Praesent eget magna in tellus semper fermentum id sit amet ipsum. Nam quis molestie ante, sed bibendum nisi.';
 
