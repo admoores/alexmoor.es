6b2f7e54e80f94bb6f43561eb5f54cba3a6e7ca6
Alex Moores <moores.alexd@gmail.com>
2017-05-14 23:44:54 -0400

    added navbar dividers

--- client/src/index/Index.jsx
+++ client/src/index/Index.jsx
@@ -9,9 +9,9 @@ class App extends React.Component {
     return (
       <div className='main'>
         <h1>Alex Moores</h1>
-        <span class="sexy_line"></span>
+        <span className="sexy_line"></span>
         <NavBar/>
-        <span class="sexy_line"></span>
+        <span className="sexy_line"></span>
         <div className='pageBody'>
           <img src="../../assets/img/headshot.jpg" className='headshot'/>
         </div>
