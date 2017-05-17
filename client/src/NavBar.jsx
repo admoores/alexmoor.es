b9181e37d1c6b59c10d84d19e17cb490b2db7bac
Alex Moores <moores.alexd@gmail.com>
2017-05-16 23:52:18 -0400

    changed navbar to contain its own dividers

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -9,16 +9,14 @@ class NavBar extends React.Component {
 
   render() {
     return (
-      <div>
-        <span className="horizontalDivider"></span>
         <div className="navBar">
-          <a href="/" className="navElement">HOME</a>
-          <a href="/resume/" className="navElement">RÉSUMÉ</a>
-          <a href="/portfolio/" className="navElement">PORTFOLIO</a>
-          <a href="/contact/" className="navElement">CONTACT</a>
+          <span className="horizontalDivider"></span>
+            <a href="/" className="navElement">HOME</a>
+            <a href="/resume/" className="navElement">RÉSUMÉ</a>
+            <a href="/portfolio/" className="navElement">PORTFOLIO</a>
+            <a href="/contact/" className="navElement">CONTACT</a>
+          <span className="horizontalDivider"></span>
         </div>
-        <span className="horizontalDivider"></span>
-      </div>
     );
   }
 }
